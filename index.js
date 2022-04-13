2718758dc9389e32ee4f1ee74341f8536bc7a3ce
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 12:32:54 -0300

    feat: iseridos middlewares de erro e validação na rota POST/user;

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const UserController = require('./controllers/UserController');
+const middlewareError = require('./middleware/middlewareError');
+const validationUser = require('./middleware/validationUser');
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/user', UserController.createUser);
+app.post('/user', validationUser, UserController.createUser);
+
+app.use(middlewareError);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
