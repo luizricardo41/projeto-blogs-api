9c78c54453073f43f12f9a5bafb1fdb0e225ce7e
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 12:42:54 -0300

    feat: middleware de erro;

---
+++ middleware/middlewareError.js
@@ -0,0 +1,9 @@
+const middlewareError = (err, _req, res, _next) => {
+  if (err.statusCode) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  console.log(err.message);
+  return res.status(500).json({ message: 'Server Error' });
+};
+
+module.exports = middlewareError;
