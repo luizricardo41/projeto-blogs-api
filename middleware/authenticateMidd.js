103b20856248ccc752523681a6a80c103e837700
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 18:04:07 -0300

    feat: criação de middleware de autenticação;

---
+++ middleware/authenticateMidd.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const unauthorized = require('../error/unauthorized');
+
+const secret = process.env.SECRET || 'mysecretkey';
+
+const authenticateMidd = (req, _res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) throw unauthorized('Token not found');
+  try {
+    jwt.verify(authorization, secret);
+    next();
+  } catch (err) {
+    throw unauthorized('Expired or invalid token');
+  }
+};
+
+module.exports = authenticateMidd;
