35af5c16ff4bdbb549d0c58b5077c2f7f04ffdae
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 19:46:06 -0300

    feat: função para busca por todas as categorias(Controller);

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,12 +4,22 @@ const createCategory = async (req, res, next) => {
   try {
     const { name } = req.body;
     const result = await CategoryService.createCategory({ name });
-    res.status(201).json(result);
+    return res.status(201).json(result);
   } catch (err) {
     next(err);
   }
 };
 
+const getCategories = async (req, res, next) => {
+  try {
+    const categories = await CategoryService.getCategories();
+    return res.status(200).json(categories);
+  } catch (err) {
+    next(err);
+  }  
+};
+
 module.exports = {
   createCategory,
+  getCategories,
 };
