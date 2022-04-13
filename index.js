7c278d6a8cf8a837f1d34e3a9d38b048967ccf1a
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 09:12:02 -0300

    feat: criação da rota POST/user;

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const UserController = require('./controllers/UserController');
 
 const app = express();
+app.use(bodyParser.json());
+
+app.post('/user', UserController.createUser);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
