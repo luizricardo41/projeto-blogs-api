233257d4cb5d748f220078b81408fbfdebc69de2
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-13 09:14:16 -0300

    feat: função para criação de novos usuarios (Services);

---
+++ services/UserService.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const conflict = require('../error/conflict');
+const { User } = require('../models');
+
+const secret = process.env.SECRET;
+
+const createUser = async (user) => {
+  const { email } = user;
+  const getUser = await User.findOne({ where: { email } });
+
+  if (getUser) throw conflict('User already registered');
+  
+  await User.create(user);
+  
+  const jwtConfig = {
+    expiresIn: '20m',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: email }, secret, jwtConfig);
+  return { token };
+};
+
+module.exports = {
+  createUser,
+};
