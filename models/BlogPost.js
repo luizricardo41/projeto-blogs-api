0fd0596bf6a859f2ec232054d24ddf5b2150f8e7
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-14 16:38:14 -0300

    fix: correção de model BlogPost;

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -3,16 +3,16 @@ const BlogPost = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
     published: DataTypes.DATE,
-    updateAt: DataTypes.DATE,
+    updated: DataTypes.DATE,
     userId: { type: DataTypes.INTEGER, foreignKey: true },
   }, {
     createdAt: 'published',
     updatedAt: 'updated', 
   });
 
-  BlogPost.associate = (models) => {
-    BlogPost.belongsTo(models.User,
-      { foreignKey: 'userId', as: 'users' });
+  BlogPosts.associate = (models) => {
+    BlogPosts.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
   };
 
   return BlogPosts;
