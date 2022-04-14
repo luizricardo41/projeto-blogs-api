bb01f924a6bf342e9f0a42041af98afdfbb07eb6
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-14 17:01:28 -0300

    feat: função para buscar todos os posts(Service);

--- services/PostService.js
+++ services/PostService.js
@@ -2,8 +2,7 @@ const jwt = require('jsonwebtoken');
 const badRequest = require('../error/badRequest');
 require('dotenv').config();
 
-const { BlogPost } = require('../models');
-const { Category } = require('../models');
+const { BlogPost, Category, User } = require('../models');
 
 const secret = process.env.SECRET || 'mysecretkey';
 
@@ -25,6 +24,19 @@ const createPost = async (post, authorization) => {
   return addNewPost;
 };
 
+const getPosts = async () => {
+  const result = await BlogPost.findAll({
+    include: [{ model: User, as: 'user' },
+      {
+        model: Category,
+        as: 'categories',
+        through: { attributes: [] },    
+    }],
+  });
+  return result;
+};
+
 module.exports = {
   createPost,
+  getPosts,
 };
