6c36df7cf4c0becae843d943b6226230f509f0f8
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-18 10:53:05 -0300

    feat: função de busca de Post por id (Controller);

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -20,7 +20,18 @@ const getPosts = async (_req, res, next) => {
   }
 };
 
+const getPostById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await PostService.getPostById(id);
+    return res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = {
   createPost,
   getPosts,
+  getPostById,
 };
