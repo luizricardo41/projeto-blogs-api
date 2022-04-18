b0316ffdf97e56249ac9b99ed90c5969b49e9fbb
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-18 16:54:36 -0300

    fix: modificado middleware de erro;

--- middleware/middlewareError.js
+++ middleware/middlewareError.js
@@ -2,6 +2,7 @@ const middlewareError = (err, _req, res, _next) => {
   if (err.statusCode) {
     return res.status(err.statusCode).json({ message: err.message });
   }
+  console.log(err.stack);
   console.log(err.message);
   return res.status(500).json({ message: 'Server Error' });
 };
